d18deb79bbd4a906661ef5c72258236788c3eaee
Bryan Garcia <ing.bryangaco@gmail.com>
2023-04-20 23:57:50 -0500

    Agregamos helper busqueda por nombre

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -1,6 +1,27 @@
-import { HeroCard } from "../components"
+import { useLocation, useNavigate } from "react-router-dom";
+import { useForm } from "../../hooks"
+import queryString from 'query-string';
+import { getHerosByName } from "../helpers";
+import { HeroCard } from "../components";
 
 export const SearchPage = () => {
+  const navigate = useNavigate();
+  const location = useLocation();
+
+  const { q = '' } = queryString.parse(location.search);
+
+  const heroes = getHerosByName(q);
+
+  const { searchText, onInputChange, onResetForm } = useForm({
+    searchText: q
+  });
+
+  const onHandleSubmit = (event) => {
+    event.preventDefault();
+    if (searchText.trim().length <= 1) return;
+    navigate(`?q=${searchText}`);
+  }
+
   return (
     <>
       <h1>SearchPage</h1>
@@ -9,13 +30,15 @@ export const SearchPage = () => {
         <div className="col-5">
           <h4>Searching</h4>
           <hr />
-          <form>
+          <form onSubmit={onHandleSubmit}>
             <input
               type="text"
               name="searchText"
               className="form-control"
               autoComplete="off"
               placeholder="Search a hero"
+              value={searchText}
+              onChange={onInputChange}
             />
             <button className="btn btn-outline-primary mt-1">
               Search
@@ -35,8 +58,11 @@ export const SearchPage = () => {
             No hero with <b>ABC</b>
           </div>
 
-          {/* <HeroCard /> */}
-
+          {
+            heroes.map(hero => (
+              <HeroCard key={hero.id} {...hero} />
+            ))
+          }
         </div>
       </div>
     </>
