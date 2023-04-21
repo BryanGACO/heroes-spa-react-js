9cc1b2c1231fe3a008c8f40a2bf81fe3bd212b9c
Bryan Garcia <ing.bryangaco@gmail.com>
2023-04-21 00:08:32 -0500

    Arrgelos search component

--- src/heroes/pages/SearchPage.jsx
+++ src/heroes/pages/SearchPage.jsx
@@ -12,13 +12,16 @@ export const SearchPage = () => {
 
   const heroes = getHerosByName(q);
 
+  const showSearch = q.length === 0;
+  const showError = q.length > 0 && heroes.length === 0;
+
   const { searchText, onInputChange, onResetForm } = useForm({
     searchText: q
   });
 
   const onHandleSubmit = (event) => {
     event.preventDefault();
-    if (searchText.trim().length <= 1) return;
+    // if (searchText.trim().length <= 1) return;
     navigate(`?q=${searchText}`);
   }
 
@@ -49,13 +52,25 @@ export const SearchPage = () => {
         <div className="col-7">
           <h4>Results</h4>
           <hr />
+          {/* {
+            (q === '')
+              ? <div className="alert alert-primary">Search a hero</div>
+              : (heroes.length === 0)
+              && <div className="alert alert-danger">No hero with <b>{q}</b></div>
+          } */}
 
-          <div className="alert alert-primary">
+          <div
+            className="alert alert-primary animate__animated animate__fadeIn"
+            style={{ display: showSearch ? "" : "none" }}
+          >
             Search a hero
           </div>
 
-          <div className="alert alert-danger">
-            No hero with <b>ABC</b>
+          <div
+            className="alert alert-danger animate__animated animate__fadeIn"
+            style={{ display: showError ? "" : "none" }}
+          >
+            No Hero with <b>{q}</b>
           </div>
 
           {
