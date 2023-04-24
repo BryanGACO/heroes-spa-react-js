c6a1d9539606c35d9a0ca5580d87eb4e300bcc96
Bryan Garcia <ing.bryangaco@gmail.com>
2023-04-23 22:50:51 -0500

    Agregamos nuevos estilos al HeroCard.jsx

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -1,5 +1,5 @@
 import { Link } from "react-router-dom";
-
+import './myHeroCard.css'
 
 const CharactersByHero = ({ alter_ego, characters }) => {
     return alter_ego !== characters ? <p>{characters}</p> : <></>;
@@ -18,32 +18,50 @@ export const HeroCard = ({
     const heroImgUrl = `/assets/heroes/${id}.jpg`;
 
     return (
-
-        <div className="col animate__animated animate__fadeIn">
-            <div className="card">
-                <div className="row no-gutters">
-                    <div className="col-4">
-                        <img src={heroImgUrl} alt={superhero} className="card-img" />
-                    </div>
-
-                    <div className="col-8">
-                        <div className="card-body">
-                            <h5 className="card-title">{superhero}
-                            </h5>
-                            <p className="card-text">{alter_ego}</p>
-
-                            <CharactersByHero characters={characters} alter_ego={alter_ego}/>
-                            <p className="card-text">
-                                <small className="text-muted">{first_appearance}</small>
-                            </p>
-
-                            <Link to={`/hero/${id}`}>
-                                Mas...
-                            </Link>
-                        </div>
+        <Link to={`/hero/${id}`} className="my-card">
+        <img src={`./assets/heroes/${id}.jpg`} className="img img-responsive" alt={superhero}/>
+        <div className="profile-name">{superhero}</div>
+        <div className="profile-position">{alter_ego}</div>
+        <div className="profile-overview">
+            <div className="profile-overview">
+                <div className="row">
+                    <div className="col-ms-4">
+                        <h3>{publisher}</h3>
+                        <p>Primera aparición: <br />{first_appearance}</p>
+                        {
+                            (alter_ego !== characters)
+                            && <p>{characters}</p>
+                        }
                     </div>
                 </div>
             </div>
         </div>
+    </Link>
+        // <div className="col animate__animated animate__fadeIn">
+        //     <div className="card">
+        //         <div className="row no-gutters">
+        //             <div className="col-4">
+        //                 <img src={heroImgUrl} alt={superhero} className="card-img" />
+        //             </div>
+
+        //             <div className="col-8">
+        //                 <div className="card-body">
+        //                     <h5 className="card-title">{superhero}
+        //                     </h5>
+        //                     <p className="card-text">{alter_ego}</p>
+
+        //                     <CharactersByHero characters={characters} alter_ego={alter_ego}/>
+        //                     <p className="card-text">
+        //                         <small className="text-muted">{first_appearance}</small>
+        //                     </p>
+
+        //                     <Link to={`/hero/${id}`}>
+        //                         Mas...
+        //                     </Link>
+        //                 </div>
+        //             </div>
+        //         </div>
+        //     </div>
+        // </div>
     )
 }
