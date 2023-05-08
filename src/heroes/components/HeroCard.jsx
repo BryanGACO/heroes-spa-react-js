0114e5456be2633c03283a07aef7fd2150e0e776
Bryan Garcia <ing.bryangaco@gmail.com>
2023-05-08 16:50:25 -0500

    Mover las imagenes a la carpeta public

--- src/heroes/components/HeroCard.jsx
+++ src/heroes/components/HeroCard.jsx
@@ -15,11 +15,9 @@ export const HeroCard = ({
     characters
 }) => {
 
-    const heroImgUrl = `/assets/heroes/${id}.jpg`;
-
     return (
         <Link to={`/hero/${id}`} className="my-card">
-        <img src={`./assets/heroes/${id}.jpg`} className="img img-responsive" alt={superhero}/>
+        <img src={`/heroes/${id}.jpg`} className="img img-responsive" alt={superhero}/>
         <div className="profile-name">{superhero}</div>
         <div className="profile-position">{alter_ego}</div>
         <div className="profile-overview">
