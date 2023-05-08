4e4c4e3bbe63200eff49b4719a04fcd9de3dc325
Bryan Garcia <ing.bryangaco@gmail.com>
2023-05-08 15:14:49 -0500

    Agregamos rutas publicas

--- src/router/routes.jsx
+++ src/router/routes.jsx
@@ -4,13 +4,16 @@ import { HeroesRoutes, routesHeroes } from '../heroes';
 import { ErrorPage } from '../ui';
 import { createBrowserRouter } from "react-router-dom";
 import { PrivateRoute } from './PrivateRoute';
+import { PublicRoute } from './PublicRoute';
 
 export const router = createBrowserRouter([
     {
         path: "login",
         errorElement: <ErrorPage />,
         element: (
-            <LoginPage />
+            <PublicRoute>
+                <LoginPage />
+            </PublicRoute>
         ),
     },
     {
