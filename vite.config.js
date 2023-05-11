9b605abdd44805bc48ad568bfe523182962bf312
Bryan Garcia <ing.bryangaco@gmail.com>
2023-05-10 20:56:47 -0500

    Arreglo a vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/heroes-spa-react-js/",
-  plugins: [react()],
+  plugins: [react()]
 })
